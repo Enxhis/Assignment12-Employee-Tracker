345a34100a15029474bd2f820d8a657d7681d2c4
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-30 14:41:19 -0500

    added schema

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "employees"
+    database: "employees_db"
 });
 
 connection.connect();
