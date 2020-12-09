084c17b5f7a0437942c7ddc5a62cbd093c542403
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-12-08 21:19:29 -0500

    final fixes

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "myloveReni2015",
+    password: "HERE GOES YOUR DATABASE PASSWORD",
     database: "employees_db"
 });
 
